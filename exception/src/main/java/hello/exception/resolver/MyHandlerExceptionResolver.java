d64e46c3011fc4c0ae72679240041fedac112cdc
Ujoonnee <dnwns0933@naver.com>
2022-12-21 16:36:18 +0900

    ~ @ControllerAdvice

--- exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -1,11 +1,9 @@
 package hello.exception.resolver;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -17,7 +15,13 @@ public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
         try {
             if (ex instanceof IllegalArgumentException) {
                 log.info("resolve IllegalArgumentException to error code 400");
+
                 response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+
+                //JSON 데이터를 직접 반환할 수 있음
+//                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//                response.setCharacterEncoding("utf-8");
+//                response.getWriter().write("{\"test\": \"ㅁㄴㅇㄹ\"}");
 
                 return new ModelAndView();
             }
